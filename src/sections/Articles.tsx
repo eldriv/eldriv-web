372d0659f56d4b5be8e3238dc335de5f233657d0
Michael Adrian Villareal <michael.adrian.villareal@valmiz.com>
2025-05-15 20:57:27 +0800

    t

--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -1,3 +1,6 @@
+// Link
+import Link from 'next/link';
+
 // Components
 import { SectionHeader } from '@/components/SectionHeader';
 import { Card } from '@/components/card';
@@ -14,56 +17,70 @@ import memojiAvatar7 from "@/assets/images/taijitu-lisp.png";
 import memojiAvatar8 from "@/assets/images/ubuntu.png";
 import { Fragment } from 'react';
 
-
-
 const Articles = [
   {
     name: "Building ’adz’ in Common Lisp with Clingon",
     topic: "Programming",
-    text: "In software development, engineers require tools that prioritize efficiency and flexibility. When dealing with complex systems, there’s a constant need for...",
+    text: "In software development, engineers require tools that prioritize efficiency and flexibility. When dealing with complex systems, there’s a...",
     avatar: memojiAvatar1,
+    href: "https://eldriv.com/en/adz",
+    target: "_blank",
   },
   {
     name: "A Wanderer's Tale of Discovering Lisp",
     topic: "Programming",
-    text: "In February 2024, I felt lost in an unfamiliar forest, where a strong aura seemed to fill the air as if someone were watching me. The path beneath my feet...",
+    text: "In February 2024, I felt lost in an unfamiliar forest, where a strong aura seemed to fill the air as if someone were watching me. The path...",
     avatar: memojiAvatar2,
+    href: "https://eldriv.com/en/lisp",
+    target: "_blank",
   },
   {
     name: "Using Tmux to Perk Up Your Terminal Experience",
     topic: "System Administration",
-    text: "As engineers, one of the essential tools we've always relied on is the terminal, due to its efficieny and speed that allows us to execute commands directly...",
+    text: "As engineers, one of the essential tools we've always relied on is the terminal, due to its efficieny and speed that allows us to execute...",
     avatar: memojiAvatar3,
+    href: "https://eldriv.com/en/tmux",
+    target: "_blank",
   },
   {
     name: "How NixOS Treated a Novice Sysadmin",
     topic: "Operating System",
-    text: "For anyone reading this article now, you might be wondering why I chose such a title. Have you ever seen someone who is very skilled in programming but struggles...",
+    text: "For anyone reading this article now, you might be wondering why I chose such a title. Have you ever seen someone who is very skilled...",
     avatar: memojiAvatar4,
+    href: "https://eldriv.com/en/nixos",
+    target: "_blank",
   },
   {
     name: "Docker Containers Are Fast",
     topic: "System Administration",
-    text: "Hello there! From the last article, I mentioned NixOS, right? My machine fully supports NixOS, and my experiences so far have exceeded my expectations...",
+    text: "Hello there! From the last article, I mentioned NixOS, right? My machine fully supports NixOS, and my experiences so far have exceeded...",
     avatar: memojiAvatar5,
+    href: "https://eldriv.com/en/docker",
+    target: "_blank",
   },
   {
     name: "Testing a Testing Framework",
     topic: "Quality Testing",
-    text: "I’ve been working with Common Lisp for about eight months now, and I’ve become curious about how to test my code in this language, similar to how it's done in...  ",
+    text: "I’ve been working with Common Lisp for about eight months now, and I’ve become curious about how to test my code in this language...",
     avatar: memojiAvatar6,
+    href: "https://eldriv.com/en/fiveam",
+    target: "_blank",
   },
   {
     name: "Explore Parsing in S-expression",
     topic: "Programming",
-    text: "Parsing is a technique where we use to analyze and understand the structure of a text or code in order to extract meaningful information. This process involves...",
+    text: "Parsing is a technique where we use to analyze and understand the structure of a text or code in order to extract meaningful...",
     avatar: memojiAvatar7,
+    href: "https://eldriv.com/en/parser",
+    target: "_blank",
   },
   {
     name: "How I Installed Ubuntu From Windows",
     topic: "Operating System",
-    text: "I have been using Windows for about 10 years, so switching to a different operating system was a brave move for me. I chose Ubuntu Linux as my new companion...",
+    text: "I have been using Windows for about 10 years, so switching to a different operating system was a brave move for me. I chose...",
     avatar: memojiAvatar8,
+    href: "https://eldriv.com/en/ubuntu",
+    target: "_blank",
   },
 ];
 
@@ -81,22 +98,25 @@ export const ArticlesSection = () => {
       {[...new Array(2)].fill(0).map((_, index) => (
       <Fragment key={index}>
       {Articles.map(articles => (
-        <Card key={articles.name} className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300">
-          <div className="flex gap-4 items-center">
-            <div className="size-14  inline-flex items-center justify-center flex-shrink-0">
-             <Image 
-             src={articles.avatar} 
-             alt={articles.name} 
-             className="max-h-full"/>
+      <Link key={articles.name} href={articles.href} target="_blank">
+          <Card className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300">
+            <div className="flex gap-4 items-center">
+              <div className="size-14 inline-flex items-center justify-center flex-shrink-0">
+                <Image 
+                  src={articles.avatar} 
+                  alt={articles.name} 
+                  className="max-h-full"
+                />
+              </div>
+              <div>
+                <div className="font-semibold text-sm text-[#fd8128]">{articles.name}</div>
+                <div className="text-sm text-white/80 mt-1">{articles.topic}</div>
+              </div>
             </div>
-          <div>
-             <div className="font-semibold text-sm text-[#fd8128]">{articles.name}</div>
-             <div className="text-sm text-white/80 mt-1">{articles.topic}</div>
-          </div>
-          </div>
-          <p className="mt-4 md:mt-6 text-sm md:text-base">{articles.text}</p>
-        </Card>
-      ))}  
+            <p className="mt-4 md:mt-6 text-sm md:text-base">{articles.text}</p>
+          </Card>
+      </Link>
+      ))}
       </Fragment>
       ))}   
       </div>
